d8320fcdeb7077c6890dcb06ea6bd619d35bd85b
Bart <kluczynskib@gmail.com>
2017-11-16 15:25:48 +0000

    added upVoting and downVoting;need to refactor api calls and move them to helpers

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import CreateMessage from './components/CreateMessage'
 import { dateFormatter } from './utils/helpers'
+import { Icon,Button } from 'semantic-ui-react'
+
 
 class App extends Component {
 
@@ -9,14 +11,19 @@ class App extends Component {
       messages: []
     }
 
-    componentDidMount(){
+    getMessages = () => {
       fetch('/api/messages')
         .then(res => res.json())
-        .then(messages => {
-          const data = messages.data
-          this.setState({ messages: data})
-        })
-      }
+          .then(messages => {
+            const data = messages.data
+            this.setState({ messages: data})
+          })
+    }
+
+
+    componentDidMount(){
+      this.getMessages();
+    }
 
       createMessage = (content) => {
             fetch('/api/messages', {
@@ -33,14 +40,43 @@ class App extends Component {
               })
               .then(x => console.log('success', x)).catch((err) => console.error('fail', err));
       }
+
+      vote = (id, option) => {
+        fetch(`/api/messages/${id}`, {
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          method: 'PUT',
+          body: JSON.stringify({
+            options: option
+          })
+        })
+        .then(() => {
+          this.getMessages();
+        })
+      }
+
+
     render() {
+
+      console.log("messages", this.state.messages)
     return (
       <div className="App">
         <h1>Messages</h1>
         <CreateMessage submit={this.createMessage} />
-        {this.state.messages.map(message =>
+        { this.state.messages && this.state.messages.map(message =>
           (
-          <div style={messageStyle} key={message.id}>{`${message.content} : ${message.timestamps}`}</div>
+          <div style={messageStyle} key={message.id}>{`${message.content} : ${message.timestamps}, current scoreVote: ${message.score}`}
+          <div>
+            <Button onClick={() => this.vote(message.id, "upVote")}>
+              <Icon name="thumbs up"/>
+            </Button>
+            <Button onClick={() => this.vote(message.id, "downVote")}>
+              <Icon name="thumbs down"/>
+            </Button>
+          </div>
+          </div>
           )
         )}
       </div>
