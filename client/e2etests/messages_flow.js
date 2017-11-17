965d4a488c32f427fd159b2fe059c9150af23feb
Bart <kluczynskib@gmail.com>
2017-11-17 11:58:19 +0000

    added test to validate downVoting the message

--- client/e2etests/messages_flow.js
+++ client/e2etests/messages_flow.js
@@ -37,6 +37,17 @@ describe('Message App', () => {
     expect(actual).to.eql(thumbs);
   })
 
+  it('Should allow any user to downVote any message and see the counter being decreased', () => {
+    const thumbs = 'Thumb awesomness: -1';
+    const message = 'Just a random message';
+    browser.url('http://localhost:3000/');
+    browser.element('#inputField').setValue(message);
+    browser.click('.submit-button');
+    browser.click('#voting :nth-child(2)');
+    const actual = browser.element('.messages_board div:nth-child(3) div:nth-child(3)').getText();
+    expect(actual).to.eql(thumbs);
+  })
+
 
 
 
